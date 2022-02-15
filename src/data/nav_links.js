54c4ca237d810d05ca5af767010cd3dc4d74c449
farah <farah.fournat@gmail.com>
2022-02-15 19:59:47 +0100

    created links array

---
+++ src/data/nav_links.js
@@ -0,0 +1,19 @@
+const links = [
+    {
+        id:1,
+        path:'#about',
+        text: 'About'
+    },
+    {
+        id:2,
+        path:'#skills',
+        text: 'Skills'
+    },
+    {
+        id:3,
+        path:'#projects',
+        text: 'Projects'
+    }
+]
+
+export default links
