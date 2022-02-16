95e03bdbeb9feb6de0bc55b23bd0280440a153b2
farah <farah.fournat@gmail.com>
2022-02-16 20:43:31 +0100

    added social links array

---
+++ src/data/social_links.js
@@ -0,0 +1,20 @@
+const socialLinks = [
+    {
+        id:1,
+        path:'https://www.linkedin.com/in/farah-fournat/',
+        icon: 'fab fa-linkedin'
+    },
+    {
+        id:2,
+        path:'https://github.com/Farahcodes',
+        icon: 'fab fa-github'
+    },
+    {
+        id:3,
+        path:'https://twitter.com/codingfarah',
+        icon: 'fab fa-twitter'
+    }
+
+]
+
+export default socialLinks
