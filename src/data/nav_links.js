a16290f16db81785ef3491221c0d9e486745a984
farah <farah.fournat@gmail.com>
2022-02-16 15:24:33 +0100

    added object to nav_links array

--- src/data/nav_links.js
+++ src/data/nav_links.js
@@ -1,16 +1,21 @@
 const links = [
     {
         id:1,
+        path:'#home',
+        text: 'Home'
+    },
+    {
+        id:2,
         path:'#about',
         text: 'About'
     },
     {
-        id:2,
+        id:3,
         path:'#skills',
         text: 'Skills'
     },
     {
-        id:3,
+        id:4,
         path:'#projects',
         text: 'Projects'
     }
