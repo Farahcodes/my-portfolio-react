08ee636e0e47ad0b7595112a01793c7767b20150
farah <farah.fournat@gmail.com>
2022-03-24 20:35:26 +0100

    added getProjects function to contentful.js

--- src/contentful.js
+++ src/contentful.js
@@ -3,3 +3,16 @@ const client = createClient({
     space:'gwykkvrc14h8',
     accessToken:'BCVVWAS79Wmf6N5d6xcXCpoGpoET7mAKlwvKVl5GHVQ'
 });
+
+
+
+export const getProjects = async()=>{
+    //gets all entries of specific content-type (project in this case)
+    const projects = await client.getEntries({
+        content_type:'project'
+    });
+    const formatted_projects= projects.items.map((project)=>{
+        console.log(project)
+    });
+    return formatted_projects
+};
