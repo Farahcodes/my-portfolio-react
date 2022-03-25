5655ef17de4223d84619d3f4f12b77ae8aee41ed
farah <farah.fournat@gmail.com>
2022-03-25 20:38:41 +0100

    corrected contentful.js

--- src/contentful.js
+++ src/contentful.js
@@ -1,18 +1,15 @@
-import { createClient } from "contentful";
+import { createClient } from 'contentful';
+
 const client = createClient({
-    space:'gwykkvrc14h8',
-    accessToken:'BCVVWAS79Wmf6N5d6xcXCpoGpoET7mAKlwvKVl5GHVQ'
+  space: 'gwykkvrc14h8',
+  accessToken: 'BCVVWAS79Wmf6N5d6xcXCpoGpoET7mAKlwvKVl5GHVQ',
 });
 
-
-
-export const getProjects = async()=>{
-    //gets all entries of specific content-type (project in this case)
-    const projects = await client.getEntries({
-        content_type:'project'
-    });
-    const formatted_projects= projects.items.map((project)=>{
-        console.log(project);
-    });
-    return formatted_projects
+export const getProjects = async () => {
+  //gets all entries of specific content-type (project in this case)
+  const projects = await client.getEntries();
+  const formatted_projects = projects.items.map((project) => {
+    console.log(project);
+  });
+  return formatted_projects;
 };
