3fc6cbe122601ed740ef68f1016e0d1ee25b96b2
farah <farah.fournat@gmail.com>
2022-03-26 14:10:43 +0100

    updated Content

--- src/contentful.js
+++ src/contentful.js
@@ -8,8 +8,8 @@ const client = createClient({
 export const getProjects = async () => {
   //gets all entries of specific content-type (project in this case)
   const projects = await client.getEntries();
-  const formatted_projects = projects.items.map((project) => {
-    console.log(project);
+  const formatted_projects = projects.items.map((Project) => {
+    console.log(Project);
   });
   return formatted_projects;
 };
