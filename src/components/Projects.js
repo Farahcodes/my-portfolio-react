c8ed0753cf9fb8db55c5452563dec53a763bec34
farah <farah.fournat@gmail.com>
2022-03-29 11:45:04 +0200

    iterating over products array

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ const Projects = ()=>{
   useEffect(()=>{
   //fetching data
   getProjects().then((projects)=>{
-    console.log(projects);
+    setProjects(projects);
   });
   },[])
     return(
@@ -21,68 +21,18 @@ const Projects = ()=>{
       </div>
      
       <div className="section-center">
-   
-        <article className="project">
-       
-          <div className="project-img">
-            
-            <img src={img} className="project-photo"/>
-          </div>
-         
-          <div className="project-info">
-            <span className="project-number">01.</span>
-            <h3>Project title</h3>
-            <p className="project-desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, incidunt eos. Nemo, quasi pariatur consequatur itaque laudantium exercitationem dicta reprehenderit.
-            </p>
-            <div className="project-stack">
-              <span>html</span>
-              <span>css</span>
-              <span>javscript</span>
+
+      {projects.map((project,index)=>{
+        const{id,img,title,description,github,completeProject,stack}= project;
+        return(
+          <article className="project" key={id}>
+            <div className="project-img">
+              <img src={img} className='project-photo' alt={title} />
             </div>
-            <div className="project-links">
-            
-              <a href="#" className="project-link">
-                <i className="fab fa-github-square"></i>
-              </a>
-              
-              <a href="#" className="project-link">
-                <i className="fas fa-share-square"></i>
-              </a>
-            </div>
-          </div>
-        </article>
-    
-        <article className="project">
-        
-          <div className="project-img">
-            
-            <img src={img} className="project-photo"/>
-          </div>
-          
-          <div className="project-info">
-            <span className="project-number">01.</span>
-            <h3>Project title</h3>
-            <p className="project-desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, incidunt eos. Nemo, quasi pariatur consequatur itaque laudantium exercitationem dicta reprehenderit.
-            </p>
-            <div className="project-stack">
-              <span>html</span>
-              <span>css</span>
-              <span>javscript</span>
-            </div>
-            <div className="project-links">
-             
-              <a href="#" className="project-link">
-                <i className="fab fa-github-square"></i>
-              </a>
-           
-              <a href="#" className="project-link">
-                <i className="fas fa-share-square"></i>
-              </a>
-            </div>
-          </div>
-        </article>
+          </article>
+        )
+      })}
+
         
 
       </div>
