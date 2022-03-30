168cbe072af78d274aaca1fda3c97bd39c43cb17
farah <farah.fournat@gmail.com>
2022-03-30 11:57:18 +0200

    added project info

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,6 +29,12 @@ const Projects = ()=>{
             <div className="project-img">
               <img src={img} className='project-photo' alt={title} />
             </div>
+
+            <div className="project-info">
+              <span className="project-number">0{index+1}.</span>
+              <h3>{title}</h3>
+              <p className="project-desc">{description}</p>
+            </div>
           </article>
         )
       })}
